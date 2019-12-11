2b44655fdca8cb2a39aeaf3272e411de3fd04a29
陆宏鸣 <lhm@jiagouyun.com>
2019-12-11 10:17:14 +0800

    k8s clear 脚本修改

--- k8s-clear.sh
+++ k8s-clear.sh
@@ -1,21 +1,21 @@
 # 清理 configmap
 kubectl delete configmaps core -n forethought-core
 kubectl delete configmaps kodo kodo-inner kodo-nginx -n forethought-kodo
-# kubectl delete configmaps message-desk message-desk-worker -n middleware
 kubectl delete configmaps front-config management-config -n forethought-webclient
+kubectl delete configmaps message-desk message-desk-worker -n middleware
 
 
 # 清理 deployment
-kubectl delete deployment front-backend inner integration-scanner manage-backend websocket -n forethought-core
-kubectl delete deployment kodo kodo-inner kodo-nginx -n forethought-kodo
-# kubectl delete deployment message-desk message-desk-worker -n middleware
-kubectl delete deployment front-webclient manage-webclient -n forethought-webclient
+kubectl delete deployments front-backend inner integration-scanner manage-backend websocket -n forethought-core
+kubectl delete deployments kodo kodo-inner kodo-nginx -n forethought-kodo
+kubectl delete deployments front-webclient manage-webclient -n forethought-webclient
+kubectl delete deployments message-desk message-desk-worker -n middleware
 
 # 清理 service
-kubectl delete service front-backend inner integration-scanner manage-backend websocket -n forethought-core
-kubectl delete service kodo kodo-inner kodo-nginx -n forethought-kodo
-# kubectl delete service message-desk message-desk-worker -n middleware
-kubectl delete service front-webclient manage-webclient -n forethought-webclient
+kubectl delete services front-backend inner integration-scanner manage-backend websocket -n forethought-core
+kubectl delete services kodo kodo-inner kodo-nginx -n forethought-kodo
+kubectl delete services front-webclient manage-webclient -n forethought-webclient
+kubectl delete services message-desk message-desk-worker -n middleware
 
 # 清理 ingress
 kubectl delete ingress front-backend manage-backend websocket -n forethought-core
