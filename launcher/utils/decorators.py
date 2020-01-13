e2ed54e5b64955b55c8d57bb6785bdd61d3c6f89
陆宏鸣 <lhm@jiagouyun.com>
2020-01-13 17:57:20 +0800

    增加升级安装检测

---
+++ launcher/utils/decorators.py
@@ -0,0 +1,24 @@
+# encoding=utf-8
+
+import subprocess, requests
+import json, functools
+
+from flask import redirect, url_for
+
+
+def upgrade_install(func):
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+    cmd = "kubectl get deploy -n forethought-core -o json"
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+
+    output, err = p.communicate()
+    deploys = json.loads(output)
+
+    # 已经有了 deploy，不是全新安装，强制跳转到升级部署页面
+    if len(deploys['items']) > 0:
+      return redirect(url_for("up_index"))
+
+    return func(*args, **kwargs)
+  return wrapper
+
