da1c7bea434c751ecb907faecd2548b7fd708391
陆宏鸣 <lhm@jiagouyun.com>
2020-05-13 14:32:59 +0800

    clear 脚本增加 data-warehouse 的清理

--- k8s-clear.sh
+++ k8s-clear.sh
@@ -5,7 +5,7 @@ kubectl delete configmaps front-web-config management-nginx-config front-nginx-c
 kubectl delete configmaps message-desk message-desk-worker -n middleware --force --grace-period=0
 kubectl delete configmaps func-config func-inner-config func-worker-config  -n func --force --grace-period=0
 kubectl delete configmaps trigger-conf -n forethought-inner-app --force --grace-period=0
-kubectl delete configmaps dataway-config dataway-license -n utils --force --grace-period=0
+kubectl delete configmaps dataway-config dataway-license data-warehouse-config -n utils --force --grace-period=0
 
 
 # 清理 deployment
@@ -15,7 +15,7 @@ kubectl delete deployments front-webclient management-webclient -n forethought-w
 kubectl delete deployments message-desk message-desk-worker nsqadmin nsqlookupd nsqd nsqd2 nsqd3 kapacitor kapacitor02 -n middleware --force --grace-period=0
 kubectl delete deployments func func-inner func-worker-beat func-worker-debugger func-worker-rpc-crontab func-worker-crontab func-worker-rpc func-worker-utils -n func --force --grace-period=0
 kubectl delete deployments trigger -n forethought-inner-app --force --grace-period=0
-kubectl delete deployments utils-server internal-dataway -n utils --force --grace-period=0
+kubectl delete deployments utils-server internal-dataway  data-warehouse -n utils --force --grace-period=0
 
 # 清理 service
 kubectl delete services front-backend inner core-worker core-worker-beat static-resource-nginx integration-scanner integration-nginx management-backend websocket -n forethought-core --force --grace-period=0
@@ -24,7 +24,7 @@ kubectl delete services front-webclient management-webclient -n forethought-webc
 kubectl delete services message-desk message-desk-worker nsqadmin nsqlookupd nsqd nsqd2 nsqd3 kapacitor kapacitor02 -n middleware --force --grace-period=0
 kubectl delete services func func-inner func-worker-beat func-worker-debugger func-worker-rpc-crontab func-worker-crontab func-worker-crontab func-worker-rpc func-worker-utils -n func --force --grace-period=0
 kubectl delete services trigger -n forethought-inner-app --force --grace-period=0
-kubectl delete services utils-server internal-dataway -n utils --force --grace-period=0
+kubectl delete services utils-server internal-dataway data-warehouse -n utils --force --grace-period=0
 
 # 清理 ingress
 kubectl delete ingress front-backend management-backend static-resource-nginx integration-nginx websocket -n forethought-core --force --grace-period=0
@@ -35,7 +35,7 @@ kubectl delete ingress func -n func --force --grace-period=0
 kubectl delete PersistentVolumeClaim ft-sysconfig core-worker-logs core-worker-beat-logs front-backend-logs inner-logs management-backend-logs integration-scanner-logs -n forethought-core --force --grace-period=0
 kubectl delete PersistentVolumeClaim kodo-logs kodo-inner-logs -n forethought-kodo --force --grace-period=0
 kubectl delete PersistentVolumeClaim  message-desk-logs message-desk-worker-logs nsqd-data-1 nsqd-data-2 nsqd-data-3 kapacitor-data-01 kapacitor-data-02 -n middleware --force --grace-period=0
-kubectl delete PersistentVolumeClaim internal-dataway-cache internal-dataway-logs  -n utils --force --grace-period=0
+kubectl delete PersistentVolumeClaim internal-dataway-cache internal-dataway-logs data-warehouse-logs  -n utils --force --grace-period=0
 
 
 # 清理 secret
