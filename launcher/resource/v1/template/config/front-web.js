c6f238302d54e716ee7dc5d6dd8d4447f2cd8f6f
陆宏鸣 <lhm@jiagouyun.com>
2020-10-13 18:22:05 +0800

    去除强制在 debug 模式域名前加上 deploy- 前缀。

--- launcher/resource/v1/template/config/front-web.js
+++ launcher/resource/v1/template/config/front-web.js
@@ -1,13 +1,13 @@
 window.DEPLOYCONFIG = {
     cookieDomain: '.{{ domain.domain }}',
-    apiUrl: "https://{{'deploy-' if __common__.debug}}{{ domain.subDomain.consoleApi }}.{{ domain.domain }}",
-    wsUrl: "wss://{{'deploy-' if __common__.debug}}{{ domain.subDomain.websocket }}.{{ domain.domain }}",
+    apiUrl: "https://{{ domain.subDomain.consoleApi }}.{{ domain.domain }}",
+    wsUrl: "wss://{{ domain.subDomain.websocket }}.{{ domain.domain }}",
     innerAppDisabled: 1,
     innerAppLogin: 'https://auth.{{ domain.domain }}/redirectpage/login',
     innerAppRegister: 'https://auth.{{ domain.domain }}/redirectpage/register',
     innerAppProfile: 'https://auth.{{ domain.domain }}/redirectpage/profile',
     innerAppCreateworkspace: 'https://auth.{{ domain.domain }}/redirectpage/createworkspace',
-    staticFileUrl: "https://{{'deploy-' if __common__.debug}}{{ domain.subDomain.staticResource }}.{{ domain.domain }}",
+    staticFileUrl: "https://{{ domain.subDomain.staticResource }}.{{ domain.domain }}",
     staticDatakit: 'https://static.dataflux.cn',
     cloudDatawayUrl: ''
 };
