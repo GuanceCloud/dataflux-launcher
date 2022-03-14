38ca71930ef18481395457e27ec05c00128ae53e
陆宏鸣 <lhm@jiagouyun.com>
2022-03-14 21:03:28 +0800

    安装完毕同步集成包到数据库

--- launcher/static/js/app.js
+++ launcher/static/js/app.js
@@ -614,6 +614,11 @@ var setup = (function () {
           that.post('studio/init').then(function(d){
             if (d.content.status_code != 200){
               alert("Studio 平台初始化失败，请刷新本页面重试。");
+            }else{
+              // 同步集成包模板到数据库
+              this.post('setting/sync_integration').done(function(d){
+
+              });
             }
           }).fail(function(d){
               alert("Studio 平台初始化失败，请刷新本页面重试。");
