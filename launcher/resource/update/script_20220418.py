0b95c111f22d6832ba1281d07d090794c4204d72
陆宏鸣 <lhm@jiagouyun.com>
2022-04-24 23:46:20 +0800

    add stdio upgrade auto step

--- launcher/resource/update/script_20220418.py
+++ launcher/resource/update/script_20220418.py
@@ -10,13 +10,40 @@ from launcher.utils.helper.db_helper import dbHelper
 from launcher import settingsMdl
 
 
-def after_container_update():
+def studio_data_upgrade():
+  print('studio data upgrade')
+  ping_url  = "http://inner.forethought-core:5000/api/v1/inner/const/ping"
+  url       = "http://inner.forethought-core:5000/api/v1/inner/upgrade/fix_data"
+
+  for i in range(0, 30):
+    content, status_code = apiHelper.get(url)
+    if status_code == 200:
+      break
+
+    time.sleep(1)
+
+  if status_code != 200:
+    return False
+  else:
+    d1 = { "script_name": "fix_2022_04_21_update_rules_funcid" }
+    d2 = { "script_name": "fix_2022_04_21_update_agg_to_metric_rule" }
+    d3 = { "script_name": "timed_sync_integration", "func_name": "timed_sync_gitee_code", "funcKwargs": {"need_sync_integration": True} }
+
+    apiHelper.post(url, data = d1)
+    apiHelper.post(url, data = d2)
+    apiHelper.post(url, data = d3)
+
+  return True
+
+
+def kodo_object_ilm_upgrade():
+  print('kodo object ilm upgrade')
   # url = "http://daily-ft2x-kodo-inner-api.cloudcare.cn/v1/ping"
   url = "http://kodo-inner.forethought-kodo:9527/v1/ping"
   for i in range(0, 30):
     content, status_code = apiHelper.get(url)
     if status_code == 200:
-        break
+      break
 
     time.sleep(1)
 
@@ -30,5 +57,13 @@ def after_container_update():
 
       if status_code != 200:
           return False
+
+  return True
+
+
+def after_container_update():
+
+  kodo_object_ilm_upgrade()
+  studio_data_upgrade()
 
   return True
