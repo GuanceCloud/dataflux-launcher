da6cdf098c65b378e935f8baf8d3b67f4ef8b9eb
陆宏鸣 <lhm@jiagouyun.com>
2022-04-25 22:11:46 +0800

    小修复

--- launcher/resource/update/script_20220418.py
+++ launcher/resource/update/script_20220418.py
@@ -16,7 +16,7 @@ def studio_data_upgrade():
   url       = "http://inner.forethought-core:5000/api/v1/inner/upgrade/fix_data"
 
   for i in range(0, 30):
-    content, status_code = apiHelper.get(url)
+    content, status_code = apiHelper.do_get(url)
     if status_code == 200:
       break
 
@@ -29,9 +29,9 @@ def studio_data_upgrade():
     d2 = { "script_name": "fix_2022_04_21_update_agg_to_metric_rule" }
     d3 = { "script_name": "timed_sync_integration", "func_name": "timed_sync_gitee_code", "funcKwargs": {"need_sync_integration": True} }
 
-    apiHelper.post(url, data = d1)
-    apiHelper.post(url, data = d2)
-    apiHelper.post(url, data = d3)
+    apiHelper.do_post(url, data = d1)
+    apiHelper.do_post(url, data = d2)
+    apiHelper.do_post(url, data = d3)
 
   return True
 
@@ -41,7 +41,7 @@ def kodo_object_ilm_upgrade():
   # url = "http://daily-ft2x-kodo-inner-api.cloudcare.cn/v1/ping"
   url = "http://kodo-inner.forethought-kodo:9527/v1/ping"
   for i in range(0, 30):
-    content, status_code = apiHelper.get(url)
+    content, status_code = apiHelper.do_get(url)
     if status_code == 200:
       break
 
