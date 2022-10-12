bf93f13cc46f3cc4c3a46c7716484afa2a250fdb
陆宏鸣 <lhm@guance.com>
2022-10-12 10:52:14 +0800

    fix 更新

--- launcher/resource/update/__init__.py
+++ launcher/resource/update/__init__.py
@@ -6,4 +6,11 @@ from . import script_20220606
 from . import script_20220929
 
 
-__all__ = ["script_1230", "script_20220418", "script_20220606", "script_20220929"]
+__all__ = [
+        "script_1230", 
+        "script_20220418", 
+        "script_20220606", 
+        "script_20220929", 
+        "script_20221012"
+        ]
+
---
+++ launcher/resource/update/script_20221012.py
@@ -0,0 +1,37 @@
+# encoding=utf-8
+
+import shortuuid
+import json
+import time
+import requests
+
+from launcher.utils.helper import api_helper as apiHelper
+
+
+def fix_script_exec():
+  print('rum black list upgrade')
+  ping_url  = "http://inner.forethought-core:5000/api/v1/inner/const/ping"
+  url       = "http://inner.forethought-core:5000/api/v1/inner/upgrade/fix_data"
+
+  for i in range(0, 60):
+    content, status_code = apiHelper.do_get(ping_url)
+    if status_code == 200:
+      break
+
+    time.sleep(1)
+
+  if status_code != 200:
+    return False
+  else:
+    # black_list fix
+    d1 = '{ "script_name": "fix_2022_09_29_blacklist_rum" }'
+
+    apiHelper.do_post(url, data = d1)
+
+  return True
+
+
+def after_container_update():
+
+  return fix_script_exec()
+
