40b7b6a39c10a2e7de7014f67fb0afa2ac4c7c24
陆宏鸣 <lhm@guance.com>
2022-11-23 16:39:52 +0800

    2011.11.17 迭代版本

--- launcher/resource/update/__init__.py
+++ launcher/resource/update/__init__.py
@@ -6,14 +6,15 @@ from . import script_20220606
 from . import script_20220929
 from . import script_20221012
 from . import script_20221020
+from . import script_20221117
 
 
 __all__ = [
-        "script_1230", 
-        "script_20220418", 
-        "script_20220606", 
-        "script_20220929", 
+        "script_1230",
+        "script_20220418",
+        "script_20220606",
+        "script_20220929",
         "script_20221012",
-        "script_20221020",
+        "script_20221117",
         ]
 
