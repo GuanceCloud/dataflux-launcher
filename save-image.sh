d3dbdb85f0d5055bd49b50e87f05a882c7c6f0ba
buleleaf <buleleaf@buleleafdeMacBook-Pro.local>
2023-04-14 16:03:34 +0800

    helm 离线包

--- save-image.sh
+++ save-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 #set +eux
 
 lastReleaseTag=$(git fetch --tag && git tag --list |awk '/release/{print $NF}'|sort -V|sed -n '$p')
@@ -65,11 +65,25 @@ guance_package (){
 
 }
 
+launcher_chart_package (){
+  list="$(dirname $0)/config/docker-image.txt"
+	version=$(sed -n "1p" ${list} |sed -r  "s/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/g")
+  helm pull  launcher  --repo https://pubrepo.guance.com/chartrepo/launcher --version=$version
+  mv launcher-$version.tgz /tmp/launcher-helm-latest-test.tgz
+}
+
+push_chart_oss (){
+  if [ -e {{/tmp/launcher-helm-latest.tgz}} ]; then
+    tools/ossutil64 cp  /tmp/launcher-helm-latest-test.tgz  oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH} -e ${GUANCE_LAUNCHER_OSS_ENDPOINT} -r -f -u --only-current-dir  -i ${GUANCE_LAUNCHER_OSS_AK_ID} -k ${GUANCE_LAUNCHER_OSS_AK_SECRET}
+  fi
+}
+
+
 push_packages_oss (){
 	  tools/ossutil64 cp  /tmp/guance-images-release  oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH} -e ${GUANCE_LAUNCHER_OSS_ENDPOINT} -r -f -u --only-current-dir  -i ${GUANCE_LAUNCHER_OSS_AK_ID} -k ${GUANCE_LAUNCHER_OSS_AK_SECRET} 
           rm -rf ${temp_dest} 
 
-	  tools/ossutil64 cp   oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH}/guance-amd64-${version}.tar.gz oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH}/guance-amd64-latest.tar.gz -e ${GUANCE_LAUNCHER_OSS_ENDPOINT}  -f -u  -i ${GUANCE_LAUNCHER_OSS_AK_ID} -k ${GUANCE_LAUNCHER_OSS_AK_SECRET} 
+	  tools/ossutil64 cp   oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH}/guance-amd64-${version}.tar.gz oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH}/guance-amd64-latest.tar.gz -e ${GUANCE_LAUNCHER_OSS_ENDPOINT}  -f -u  -i ${GUANCE_LAUNCHER_OSS_AK_ID} -k ${GUANCE_LAUNCHER_OSS_AK_SECRET}
 
 	  tools/ossutil64 cp   oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH}/guance-arm64-${version}.tar.gz oss://${GUANCE_LAUNCHER_OSS_BUCKET}/${GUANCE_LAUNCHER_OSS_PATH}/guance-arm64-latest.tar.gz -e ${GUANCE_LAUNCHER_OSS_ENDPOINT}  -f -u  -i ${GUANCE_LAUNCHER_OSS_AK_ID} -k ${GUANCE_LAUNCHER_OSS_AK_SECRET}
 }
@@ -85,8 +99,12 @@ do
             echo "do packages"
             guance_package arm64
             guance_package amd64
-	    push_packages_oss 
+	          push_packages_oss
             ;;
+        c)
+            launcher_chart_package
+            push_chart_oss
+        ;;
         ?)
             echo "-t: add a tag to deploy trigger"
             exit 1
